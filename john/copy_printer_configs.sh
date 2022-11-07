f38ae596bd9d7f4e1669ce23493af2b965cf6353
John Heaton <jheaton.dev@gmail.com>
2022-11-07 18:26:48 -0500

    Remove personal configs

--- john/copy_printer_configs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Prune old configs
-rm -f ~/klipper_config/printer_*/printer-*.cfg
-
-# Copy latest config
-rsync -r ~/klipper_config/printer_* .
-rsync --ignore-missing-args ~/klipper_config/webcam.txt .
